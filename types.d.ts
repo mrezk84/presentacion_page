6237e7c1702ad0676079a792ef6f753f3472808a
Marcos Rezk <marcosrezk@192.168.1.188>
2025-05-22 19:43:55 -0300

    pagina inicial presentación Astro
    Se genera proyecto en Astro para presentación personal

---
+++ types.d.ts
@@ -0,0 +1,23 @@
+// types.d.ts
+
+declare module 'aos' {
+  interface AOSOptions {
+    offset?: number;
+    delay?: number;
+    duration?: number;
+    easing?: string;
+    once?: boolean;
+    mirror?: boolean;
+    anchorPlacement?: string;
+  }
+
+  interface AOS {
+    init(options?: AOSOptions): void;
+    refresh(): void;
+    refreshHard(): void;
+    destroy(): void;
+  }
+
+  const aos: AOS;
+  export default aos;
+}
